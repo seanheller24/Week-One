317651e3d1ab3d457fcb0b0ac5ff0ff88cf2aeaf
seanheller24 <hellerse@bc.edu>
2022-01-24 18:43:45 -0500

    Week One Class Work

--- Week One/ViewController.swift
+++ Week One/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var showMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func sayItButtonPressed(_ sender: UIButton) {
+        showMessageLabel.text = "Sick!"
+    }
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        showMessageLabel.text = "Fabulous? That's You!!"
+    }
+    
 }
 
